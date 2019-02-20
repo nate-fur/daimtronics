9073b15f106d37557b6c8497b565e6baad465057
Nate Furbeyre <nfurbeyr@calpoly.edu>
2019-02-20 06:00:15 +0000

    removed old files

--- build/ros-lidar-lite-master/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ubuntu/catkin_ws/install/include".split(';') if "/home/ubuntu/catkin_ws/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-llidar_lite".split(';') if "-llidar_lite" != "" else []
-PROJECT_NAME = "lidar_lite"
-PROJECT_SPACE_DIR = "/home/ubuntu/catkin_ws/install"
-PROJECT_VERSION = "1.0.0"
