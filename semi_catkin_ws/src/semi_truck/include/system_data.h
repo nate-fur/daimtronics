2b457d26ee468657d8fe9dd47c9d96c4387b0b89
Nathaniel Furbeyre <nmfurbearr@outlook.com>
2019-05-16 19:33:15 -0700

    fixed semicolon error

--- semi_catkin_ws/src/semi_truck/include/system_data.h
+++ semi_catkin_ws/src/semi_truck/include/system_data.h
@@ -24,8 +24,8 @@ struct system_data_t {
    uint16_t  right_TOF;     // distance of nearest object for the right TOF
    uint16_t  left_TOF;      // distance of nearest object for the left TOF
    uint16_t  rear_TOF;      // distance of nearest object for the rear TOF
-   uint16_t  drive_mode_1
-   uint16_t  drive_mode_2
+   uint16_t  drive_mode_1;
+   uint16_t  drive_mode_2;
    int16_t   motor_output;   // output sent to the motor driver
    int16_t   steer_output;   // output sent to the steering servo
    uint16_t  fifth_output;  // actual state of the 5th wheel
